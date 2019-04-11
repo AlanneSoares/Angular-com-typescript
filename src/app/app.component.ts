3380c2deaada93e695ebebe0a6f09fda24c9dff8
Alanne Soares <alannesilva@yahoo.com.br>
2019-04-11 01:22:16 -0300

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'seu primeiro projeto Angular!';
+  title = 'Seja bem-vindo(a) ao Angular!';
 }
